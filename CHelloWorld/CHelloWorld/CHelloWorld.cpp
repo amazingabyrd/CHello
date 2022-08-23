a6eb912af05ef7940b6c97d57b3bd9187709b798
amazingabyrd <austinybyrdy@gmail.com>
2022-08-23 15:41:36 -0600

    HelloWorld
    Completed

--- CHelloWorld/CHelloWorld/CHelloWorld.cpp
+++ CHelloWorld/CHelloWorld/CHelloWorld.cpp
@@ -1,20 +1,68 @@
-// CHelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+* The program randomly generates numbers
+* between 1 and 1000 for the user to guess.
+*/
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+
+#define MIN_NUMBER 1
+const int MAX_NUMBER = 100;
+
+void guessGame(void);
+int isCorrect(int, int);
+
+
+int main(void)
 {
-    std::cout << "Hello World!\n";
+    srand(time(0));
+    guessGame();
+
+    return 0;
+} // end main
+
+
+void guessGame(void)
+{
+    int answer = 0;
+    int guess = 0;
+    int response = 0;
+
+    do {
+        answer = MIN_NUMBER + rand() % MAX_NUMBER;
+
+        printf("Hello, I have a number between %d and %d .\n"
+            "Can you guess my number?\n"
+            "Please type your first guess.", MIN_NUMBER, MAX_NUMBER);
+
+        printf("%s", "? ");
+        scanf("%d", &guess);
+
+        while (!isCorrect(guess, answer))
+            scanf("%d", &guess);
+
+        puts("\nExcellent! You guessed the number!\n"
+            "Would you like to play again?");
+        printf("%s", "Please type ( 1=yes, 2=no )? ");
+        scanf("%d", &response);
+
+        puts("");
+    } while (response == 1);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int isCorrect(int g, int a)
+{
+    if (g == a) {
+        return 1;
+    }
+
+    if (g < a)
+        printf("%s", "Too low. Try again.\n? ");
+    else
+        printf("%s", "Too high. Try again.\n? ");
+
+    return 0;
+}
