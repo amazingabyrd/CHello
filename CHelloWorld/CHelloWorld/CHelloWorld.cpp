2b224b0a6f63e389ebf14141bd07d583e810ea13
amazingabyrd <austinybyrdy@gmail.com>
2022-08-25 16:32:15 -0600

    Good work on HW1

--- CHelloWorld/CHelloWorld/CHelloWorld.cpp
+++ CHelloWorld/CHelloWorld/CHelloWorld.cpp
@@ -1,7 +1,29 @@
 #include <stdio.h>
-int main() {
 
-	printf("You know it");
+int main( void ) {
+	int length = 0;
+	int width = 0;
+	int area = 0;
+
+	printf("%s", "Enter the length: ");
+	scanf("%d", &length);
+
+	printf("%s", "Enter the width: ");
+	scanf("%d", &width);
+
+	area = length * width;
+
+	printf("%s", "Length: ");
+	printf("%d",length);
+	printf("%s", " Width: ");
+	printf("%d", width);
+	printf("%s", " Area: ");
+	printf("%d", area);
+
+	int div = 0;
+	div = length / width;
+	printf("%s", " Length divided by width: ");
+	printf("%d", div);
 
 	return 0;
 }
