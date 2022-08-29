cc8054f4bda0087f8850f2939b7cea0834d95bed
amazingabyrd <austinybyrdy@gmail.com>
2022-08-29 14:53:37 -0600

    Newday

--- CHelloWorld/CHelloWorld/CHelloWorld.cpp
+++ CHelloWorld/CHelloWorld/CHelloWorld.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
+bool boolean(int x, int y);
 
 int main( void ) {
-	int length = 0;
-	int width = 0;
-	int area = 0;
+	//variables
+	int length, width, area, div = 0;
 
+
+	//get data
 	printf("%s", "Enter the length: ");
 	scanf("%d", &length);
 
@@ -12,18 +14,41 @@ int main( void ) {
 	scanf("%d", &width);
 
 	area = length * width;
+	div = length / width;
 
-	printf("%s", "Length: ");
-	printf("%d",length);
+	//output data
+	printf("%s","Length: ");
+	printf("%d", length);
+
 	printf("%s", " Width: ");
 	printf("%d", width);
+
 	printf("%s", " Area: ");
 	printf("%d", area);
 
-	int div = 0;
-	div = length / width;
 	printf("%s", " Length divided by width: ");
 	printf("%d", div);
 
+	if (length != width) {
+		printf(" \n %d is not equal to %d", length, width);
+	}
+	else printf(" \n %d is equal to %d", length, width);
+	
+	for (int i = 0; i < 10; i++) {
+		printf("this is working \n");
+	}
+	int n = 0;
+	while (n < 10) {
+		printf("this is also working\n");
+		n++;
+	}
+
+	int x = boolean(1, 4);
+	printf("%d", x);
 	return 0;
+}//end of main
+
+bool boolean(int x, int y) {
+	if (x < y)return 1;
+	else return 0;
 }
