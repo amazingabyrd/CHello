e86ca0e58e609c576e042c16a1c523d4bb97e95d
amazingabyrd <austinybyrdy@gmail.com>
2022-09-21 19:12:01 -0600

    done

--- ByrdAustinHW03.c
+++ ByrdAustinHW03.c
@@ -7,7 +7,6 @@ Implement a Maintainable Quality Solution
 
 //preprocessor directives
 #include <stdio.h>
-
 #include <math.h>
 
 //declare global constants for modularity
@@ -29,7 +28,6 @@ int main()
 	//Declare input variable 
 	float inputtemp;
 	float input;
-	int scannVar;
 	int sum = 0; //totalprice
 	int counter = 1; //boards rented
 	unsigned int time = 1;
@@ -40,8 +38,10 @@ int main()
 		printf("%s", "Enter the number of hours the board was rented or enter -1 to quit.\n");
 
 		//recieve input
-		scannVar = scanf("%10f", &inputtemp);
-		while ((getchar()) != '\n');
+		scanf("%10f", &inputtemp);
+		//check invalid character input
+		while ((getchar()) != '\n'); 
+		//round input time to next highest hour
 		input = ceil(inputtemp);
 		
 
@@ -50,6 +50,7 @@ int main()
 		if (checkValidity(input) == 0 ) 
 		{
 			printf("%s", "\aError: Enter the time rented. (From 0.1 to 10 hours)\n");
+			continue;// I know, I know you said not to, but this fixes problems; if you feel like it isn't please chastise me in class by explaining very clearly what to do instead
 		}
 		//continue program if input is valid and not -1
 		else if (checkValidity(input) == 1)
@@ -57,25 +58,27 @@ int main()
 			sum += price(input);
 			display(price(input), inputtemp, counter);
 		}
-		//if -1 is entered continue and break while loop
+		//if the sentinel value (-1) is entered
 		else
 		{
 
 		}
-		
 
 		//increment boards and time rented
 		counter++;
 		time += input;
 	} while (input != -1);
-
+	//if no boards were rented, display message
+	if (counter == 2) 
+	{
+		printf("%s", "No boards were rented today.\n");
+	}
 
 	//prints final data and PRICE, corrects counter from final loop increments
 	display(sum, time, counter-2);
-
 }
 
-// Function checks inputs validity
+// Function checks inputs numerical validity
 int checkValidity(float hours)
 {
 	if (hours == -1)
@@ -118,6 +121,6 @@ float price(float hours)
 // Prints final price and data for user after hours have been entered
 void display(int total, float time, int count)
 {
-	printf("%s", "Board\tHours\t\tCharge\n");
-	printf("%d\t%f\t%d\n", count, time, total);
+	printf("%s", "Board\tHours\tCharge\n");
+	printf("%d\t%1.2f\t$%d\n", count, time, total);
 }
